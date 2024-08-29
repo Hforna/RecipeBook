21a58d816f12916b579b35d3293f8f38de9b090a
Hforna <henriqueflashpingo@gmail.com>
2024-08-29 10:30:40 -0300

    add dependency injection in application

---
+++ src/backend/ProjectAspNet.Application/DependencyInjectionApplication.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectAspNet.Application.Services.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAspNet.Application
+{
+    public static class DependencyInjectionApplication
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            AddMapper(services);
+        }
+
+        public static void AddMapper(IServiceCollection services)
+        {
+            var mapper = new AutoMapper.MapperConfiguration(x => { x.AddProfile(new UserMappper()); }).CreateMapper();
+            services.AddScoped(opt => mapper);
+        }
+    }
+}
