bca3d2d01c93d4bf994486bcb9995f205c516bdf
Hforna <henriqueflashpingo@gmail.com>
2024-08-29 20:10:33 -0300

    add lanaguage middlewares

---
+++ src/backend/ProjectAspNet/Middlewares/CultureInfoMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ProjectAspNet.Middlewares
+{
+    public class CultureInfoMiddleware
+    {
+        public RequestDelegate _next;
+        public CultureInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var acceptedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            var requestLanguage = context.Request.Headers.AcceptLanguage;
+            var currentCulture = new CultureInfo("us");
+
+            if(string.IsNullOrEmpty(requestLanguage) == false && acceptedLanguages.Any(x => x.Equals(requestLanguage)))
+            {
+                currentCulture = new CultureInfo(requestLanguage!);
+            }
+            CultureInfo.CurrentCulture = currentCulture;
+            CultureInfo.CurrentUICulture = currentCulture;
+
+            await _next(context);
+        }
+    }
+}
