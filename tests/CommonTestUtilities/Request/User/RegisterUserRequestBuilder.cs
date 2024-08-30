c395d8a4b9ae2630007a269681f998210106f62b
Hforna <henriqueflashpingo@gmail.com>
2024-08-30 19:55:34 -0300

    start unit tests with xUnit and bogus for fake datas

---
+++ tests/CommonTestUtilities/Request/User/RegisterUserRequestBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using ProjectAspNet.Communication.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTestUtilities.Request.User
+{
+    public class RegisterUserRequestBuilder
+    {
+        public static RegisterUserRequest Create()
+        {
+            return new Faker<RegisterUserRequest>()
+                .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(user => user.Password, (f) => f.Internet.Password());
+        }
+    }
+}
---
+++ tests/Validators/RegisterUserRequestTest.cs
@@ -0,0 +1,25 @@
+﻿using CommonTestUtilities.Request.User;
+using FluentAssertions;
+using ProjectAspNet.Application.UseCases.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validators
+{
+    public class RegisterUserRequestTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var request = RegisterUserRequestBuilder.Create();
+            var validator = new UserRegisterValidate();
+            request.Name = string.Empty;
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+        }
+    }
+}
