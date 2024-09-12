0f5cdc746604b0fad343d822d53828bbd1a7c377
Hforna <henriqueflashpingo@gmail.com>
2024-09-11 22:37:58 -0300

    add respositories for tokens

---
+++ src/backend/ProjectAspNet.Domain/Repositories/Security/Tokens/ITokenValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAspNet.Domain.Repositories.Security.Tokens
+{
+    public interface ITokenValidator
+    {
+        public Guid Validate(string token);
+    }
+}
---
+++ src/backend/ProjectAspNet.Domain/Repositories/Users/IUserIdentifierExists.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectAspNet.Domain.Repositories.Users
+{
+    public interface IUserIdentifierExists
+    {
+        public Task<bool> UserIdentifierExists(Guid uid);
+    }
+}
