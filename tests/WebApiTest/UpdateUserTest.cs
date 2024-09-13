88b77af2a64a4f724e19490ade0e10e23229b941
Hforna <henriqueflashpingo@gmail.com>
2024-09-13 18:27:58 -0300

    test error email exists in integration update user test

--- tests/WebApiTest/UpdateUserTest.cs
+++ tests/WebApiTest/UpdateUserTest.cs
@@ -3,6 +3,7 @@ using CommonTestUtilities.Repositories;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using ProjectAspNet.Communication.Requests;
+using ProjectAspNet.Exceptions.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,6 +34,21 @@ namespace WebApiTest
             _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", generate.Generate(_factory.getUserIdentifier()));
             var response = await _httpClient.PutAsJsonAsync("user", request);
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+        }
+
+        [Fact]
+        public async Task Error_Email_Exists()
+        {
+            var generate = JwtTokenGenerate.Build();
+            var user = UserEntitieTest.Build();
+            _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", generate.Generate(_factory.getUserIdentifier()));
+            var request = new RequestUpdateUser() { Name = user.user.Name, Email = _factory.getEmail() };
+            var response = await _httpClient.PutAsJsonAsync("user", request);
+            var readAsStream = response.Content.ReadAsStream();
+            var responseContent = JsonDocument.Parse(readAsStream);
+            var listErrors = responseContent.RootElement.GetProperty("errors").EnumerateArray();
+            var errors = listErrors.Select(e => e.GetString()).ToList();
+            errors.Should().Contain(ResourceExceptMessages.EMAIL_ALREADY_EXISTS);
         }
     }
 }
