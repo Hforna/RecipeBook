39ffc6b6bc3309c20cb80e7e2354be2b90505173
Hforna <henriqueflashpingo@gmail.com>
2024-10-29 23:19:23 -0300

    add data annotations to entities

--- src/backend/ProjectAspNet.Domain/Entities/Recipes/Recipe.cs
+++ src/backend/ProjectAspNet.Domain/Entities/Recipes/Recipe.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectAspNet.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,13 +12,17 @@ namespace ProjectAspNet.Domain.Entities.Recipes
     [Table("recipes")]
     public class Recipe : BaseEntitie
     {
+        [MaxLength(255, ErrorMessage = "Field must have only 255 digits or less")]
         public string? Title { get; set; }
+        [MaxLength(1, ErrorMessage = "Invalid time recipe type")]
         public CookingTime? TimeRecipe { get; set; }
+        [MaxLength(1, ErrorMessage = "Invalid difficulty type")]
         public Difficulty? Difficulty { get; set; }
         public IList<InstructionsEntitie> Instructions { get; set; } = [];
         public IList<DishTypeEntitie> DishTypes { get; set; } = [];
         public IList<IngredientEntitie> Ingredients { get; set; } = [];
         public string? ImageIdentifier { get; set; }
+        [ForeignKey("users")]
         public long UserId { get; set; }
     }
 }
--- src/backend/ProjectAspNet.Domain/Entities/UserEntitie.cs
+++ src/backend/ProjectAspNet.Domain/Entities/UserEntitie.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectAspNet.Domain.Entities
 {
-    public class UserEntitie : BaseEntitie
+    [Table("users")]
+    public class UserEntitie : IdentityUser<long>
     {
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public bool Active { get; set; } = true;
+        [MaxLength(255, ErrorMessage = "Name field must be less than 255")]
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        [EmailAddress(ErrorMessage = "Invalid email field")]
+        public override string Email { get; set; } = string.Empty;
+        [MinLength(8, ErrorMessage = "Password must have 8 or more digits")]
         public string Password {  get; set; } = string.Empty;
         public Guid UserIdentifier { get; set; } = Guid.NewGuid();
     }
